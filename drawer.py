bb5e2e22940380d326f0be124c63130c36b2806a
Laurent Chasserat <laurent.chasserat@gmail.com>
2021-08-01 18:27:47 +0200

    Add method to draw a polygon

--- drawer.py
+++ drawer.py
@@ -7,6 +7,14 @@ class Drawer:
     def __init__(self, *args, **kwargs):
         pass
 
+    def draw_polygon(self, context, points):
+        context.move_to(points[0][0], points[0][1])
+        for x, y in points[1:]:
+            context.line_to(x, y)
+
+        context.close_path()
+        context.stroke()
+
     def draw_picture(self):
         print('Now drawing a cool picture.')
         width = 600
@@ -19,7 +27,7 @@ class Drawer:
             context = cairo.Context(surface)
 
             context.scale(width, height)
-            context.set_line_width(0.01)
+            context.set_line_width(0.005)
 
             # Fill background
             context.set_source_rgb(1, 1, 1)
@@ -29,12 +37,9 @@ class Drawer:
 
             # Draw polygon
             context.set_source_rgb(0, 0, 0)
-            context.move_to(x1, y1)
-            context.line_to(x2, y2)
-            context.line_to(x3, y3)
-            context.line_to(x4, y3)
-            context.line_to(x1, y1)
-            context.stroke()
+            print(x1)
+            self.draw_polygon(context, [(x1, y1), (x2, y2), (x3, y3), (x4, y4)])
+            self.draw_polygon(context, [(0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4)])
 
             # Save as a SVG and PNG
             surface.write_to_png('example.png')
